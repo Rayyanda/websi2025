284f5a10397cac2fd91bbc2de62fb33449363893
Rayyanda <rayyanda5113@gmail.com>
2025-05-10 10:08:10 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name'=> 'Eka Yuni',
+            'email' => 'ekayuniastuty@yahoo.com',
+            'password'=> Hash::make('adminwebsi123'),
+            'role'=> 'admin'
         ]);
     }
 }
